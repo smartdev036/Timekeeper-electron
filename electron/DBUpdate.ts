ddb07d4dcd25a889fe5a1d22a054e15c681a50c1
smart <smart@no-reply.com>
2023-03-17 15:52:05 -0500

    initial db update code

--- electron/DBUpdate.ts
+++ electron/DBUpdate.ts
@@ -1,4 +1,5 @@
 import { Close, Open } from "./Models/Core";
+import { AsObject } from "./Models/Helpers";
 
 const DBUpdate = async () => {
 	const db = await Open();
@@ -39,10 +40,34 @@ const DBUpdate = async () => {
 		console.log("new tbl_pos_combined_integration creation error");
 	}
 
+	// DB structure update with start_time to start_minute
 	try {
-		db.exec(`
-			ALTER TABLE log_times ADD start_minute INTEGER DEFAULT 0
+		const result = db.exec(
+			"SELECT COUNT(*) as value FROM pragma_table_info('log_times') WHERE name = 'start_minute';"
+		);
+		const resObj = AsObject<any>(result);
+		const isExistMinute = resObj[0].value;
+		// console.log("isExistMinute: ", isExistMinute);
+
+		if (!isExistMinute) {
+			db.exec(`
+				CREATE TABLE if not exists new_log_times (
+					position_id INTEGER CHECK( position_id > 0 ) NOT NULL,
+					controller_id INTEGER NOT NULL,
+					trainee_controller_id INTEGER NOT NULL,
+					log_date TEXT NOT NULL,
+					start_time INTEGER NOT NULL,
+					start_minute INTEGER NOT NULL,
+					PRIMARY KEY (position_id, log_date, start_time)
+				);
+			
+				INSERT INTO new_log_times (position_id, controller_id, trainee_controller_id, log_date, start_time, start_minute)
+				SELECT position_id, controller_id, trainee_controller_id, log_date, start_time, Floor(start_time/60)*60 FROM log_times;
+				
+				drop table log_times;
+				ALTER table new_log_times RENAME TO log_times;
 			`);
+		}
 	} catch (err) {
 		console.log("new tbl_pos_combined_integration creation error");
 	}
